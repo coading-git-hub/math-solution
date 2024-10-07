6cdb7bbf2ad3f19beafa9b319ab19431bb980b5d
KIRAN AHMED <162012113+coading-git-hub@users.noreply.github.com>
2024-10-08 00:40:15 +0500

    Update index.js
    remove extra

--- index.js
+++ index.js
@@ -2,14 +2,11 @@
 
 for(var i = 1; i <= 100; i = i+10){
          
-for(var i = 1; i <= 100; i += 10){
- 
-        
 for(var j= i; j < i + 10; j++ ){
     document.write(" " +" " +  j + "  ");
 
 }
-document.write("</br></br>")}}
+document.write("</br></br>")}
 
 ////////////////////////////////////////////////////////////
 
@@ -18,16 +15,13 @@ document.write("</br></br>")}}
 //////////////////////////////////////////////
 document.write(`<div><h1 style="text-align: center; background-color: aqua; margin-top: 5%;">BACKWORD COUNTING</h1></div></br>`)
 for(var i = 10 ; i>=1 ; i--){
-    
-
-for(var i = 100; i >= 1; i -= 10){
 
 for(var j= i; j > i - 10; j-- ){
     document.write(" "+" " +  j + " " );
 
 }
 document.write("</br></br>")
-}}
+}
 
 /////////////////table/////////////
 
